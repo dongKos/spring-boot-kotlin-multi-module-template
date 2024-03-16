919a27d0f4dcc199919fdcc21fd15ffe3fa0ff30
김동환 <dhkim931101@naver.com>
2024-03-16 22:41:56 +0900

    chore: hide health controller

--- api/query/src/main/kotlin/com/example/demo/HealthController.kt
+++ api/query/src/main/kotlin/com/example/demo/HealthController.kt
@@ -1,11 +1,13 @@
 package com.example.demo
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Hidden
 @RestController
-class HeathController() {
+class HealthController {
     @Value("\${spring.profiles.active:none}")
     lateinit var activeProfiles: String
 
